e92e4a032015811e9bb260faec1d7e068c071788
jdrake16 <stovetops111@gmail.com>
2020-11-30 16:41:31 -0700

    Basic list view with toast setup

--- app/src/main/java/com/example/listviewapp/MainActivity.java
+++ app/src/main/java/com/example/listviewapp/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.listviewapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -27,9 +31,21 @@ public class MainActivity extends AppCompatActivity {
         peeps.add("Tesla");
         peeps.add("Chappie");
 
+        //alternate way to do a list...
+        //ArrayList<String> peeps = new ArrayList<String>(asList("Morgan", "Simba", "Atlas"));
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, peeps);
         listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Log.i("doggo tapped:", peeps.get(position));
+
+                Toast.makeText(getApplicationContext(), "Ollo " + peeps.get(position), Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
 
     }
